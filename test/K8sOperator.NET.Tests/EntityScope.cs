7b9a10ada4330b44ad2190f29f5e16377e26dd99
Patrick Evers <pmdevers@gmail.com>
2024-09-10 15:22:30 +0200

    Change between cluster and namespaced client

---
+++ test/K8sOperator.NET.Tests/EntityScope.cs
@@ -0,0 +1,50 @@
+﻿using K8sOperator.NET.Extensions;
+using K8sOperator.NET.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K8sOperator.NET.Tests;
+public class EntityScope_Tests
+{
+    [Fact]
+    public void NamespacedScope_should_use_NamespacedClient()
+    {
+        var builder = OperatorHost.CreateOperatorApplicationBuilder();
+
+        builder.AddController<TestController>()
+            .WatchNamespace("test");
+
+        var app = builder.Build();
+
+        var watcher = app.DataSource.GetWatchers(app.ServiceProvider).ToList();
+
+        watcher.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public void ClusterScope_should_use_NamespacedClient()
+    {
+        var builder = OperatorHost.CreateOperatorApplicationBuilder();
+
+        builder.AddController<Test2Controller>();
+        
+        var app = builder.Build();
+
+        var watcher = app.DataSource.GetWatchers(app.ServiceProvider).ToList();
+
+        watcher.Should().HaveCount(1);
+    }
+
+    private class TestController : Controller<TestResource> { }
+
+    private class Test2Controller : Controller<ClusterResource> { }
+
+    [EntityScopeMetadata(EntityScope.Cluster)]
+    public class ClusterResource : TestResource
+    {
+
+    }
+}
